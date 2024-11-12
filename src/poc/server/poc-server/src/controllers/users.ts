0f553c5ab3126e7151d9f4086707736f0ceed268
Kyle D'Souza <kyle49008@gmail.com>
2024-11-12 00:22:35 -0500

    add route for checking user location

--- src/poc/server/poc-server/src/controllers/users.ts
+++ src/poc/server/poc-server/src/controllers/users.ts
@@ -1,7 +1,12 @@
-import {v4 as uuidv4} from "uuid";
+import { v4 as uuidv4 } from "uuid";
 import { Request, Response } from "express";
+import haversine from 'haversine-distance';
 import { io } from "../index";
 export const sessionIds: string[] = [];
+export const ITBLocation = {
+    latitude: 43.25898815315478,
+    longitude: -79.92086378832343
+};
 
 /**
  * Route to create session.
@@ -12,7 +17,50 @@ export const createSession = async (req: Request, res: Response) => {
     const uuid: string = uuidv4();
     console.log(uuid);
     sessionIds.push(uuid);
-    res.status(200).json({uuid});
+    res.status(200).json({ uuid });
+}
+
+/**
+ * Route to verify a users location
+ * @name /api/users/verify-location
+ * @function
+ */
+export const verifyLocation = async (req: Request, res: Response) => {
+    const uuid: string = req.body.userId;
+    try {
+        if (!sessionIds.includes(uuid)) {
+            res.status(401).json({ message: "User does not exist" });
+            return;
+        }
+
+        console.log(
+            "User [%s] exists at [%s] longitude and [%s] latitude",
+            uuid,
+            req.body.longitude,
+            req.body.latitude
+        );
+        const userLocation = {
+            latitude: req.body.latitude,
+            longitude: req.body.longitude
+        };
+
+        const distance: number = haversine(userLocation, ITBLocation)
+
+        console.log("User [%s], is [%d] meters from the site", uuid, distance)
+
+        if (distance > 10) {
+            console.log(
+                "User [%s], is too far from the site, failed to authenticate",
+                uuid
+            );
+            res.status(401).json({ message: "User is not on a registered site" });
+            return;
+        }
+        res.status(200).json({ uuid });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 }
 
 /**
@@ -24,5 +72,5 @@ export const createSession = async (req: Request, res: Response) => {
 export const emitConnection = async (req: Request, res: Response) => {
     const { uuid } = req.body;
     io.to(uuid).emit("event");
-    res.status(200).json({message: "Connection established"});
+    res.status(200).json({ message: "Connection established" });
 }
