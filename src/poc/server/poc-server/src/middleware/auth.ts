638dd1a8ff9db3e3bfef3753d725ec462776dbf3
spitgranger <richard.fan2000@hotmail.com>
2024-11-14 17:00:32 -0500

    fix(poc): missing encoding of secret key + default export #275

--- src/poc/server/poc-server/src/middleware/auth.ts
+++ src/poc/server/poc-server/src/middleware/auth.ts
@@ -7,7 +7,7 @@ const auth = async (req: Request, res: Response, next: NextFunction) => {
         const token = req?.headers?.authorization?.split(" ")[1];
         if (token !== undefined) {
             let decodedData;
-            decodedData = await jwtVerify(token, createSecretKey(process.env.JWT_SECRET || "1234567891011121314151617181920212223242526272829303132"));
+            decodedData = await jwtVerify(token, createSecretKey(process.env.JWT_SECRET || "1234567891011121314151617181920212223242526272829303132", 'utf-8'));
             console.log(decodedData);
             next();
         } else {
@@ -17,6 +17,8 @@ const auth = async (req: Request, res: Response, next: NextFunction) => {
     } catch (error) {
         console.log(error);
         res.status(403).json("must be logged in and/or invalid token");
-        return
+        return;
     }
 }
+
+export default auth;
