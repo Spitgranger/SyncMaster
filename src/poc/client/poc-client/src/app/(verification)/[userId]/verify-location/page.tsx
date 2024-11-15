5fd4ae792cbfe8ffc9b8ee8cf6b57330bd00ee2e
akshitg3 <akshit.gulia@outlook.com>
2024-11-15 11:04:06 -0500

    Added location prompt mobile

--- src/poc/client/poc-client/src/app/(verification)/[userId]/verify-location/page.tsx
+++ src/poc/client/poc-client/src/app/(verification)/[userId]/verify-location/page.tsx
@@ -9,21 +9,17 @@ import socket from '@/utils/socket';
 
 const VerifyLocation = () => {
     const params = useParams<{ userId: string }>()
-    const [lat, setLat] = useState("0");
-    const [long, setLong] = useState("0");
     const [isVerified, setIsVerified] = useState(false);
     const userId = params.userId;
     useSocket();
     socket.on('authSuccess', () => {
+        setIsVerified(true);
     })
     const handleVerifyLocation = async () => {
-        // const { lat: lat, long: long } = await getGeolocation();
-        let latitude = 43.25898815315478;
-        let longitude = -79.92086378832343;
-        try{
-            await verifyLocation(userId, latitude, longitude);
-            setIsVerified(true);
-        }catch(err){
+        const { lat: lat, long: long } = await getGeolocation();
+        try {
+            await verifyLocation(userId, lat, long);
+        } catch (err) {
             console.log(err)
         }
 
@@ -37,7 +33,7 @@ const VerifyLocation = () => {
                 <p>Location Verified Successfully</p>
                 <br />
                 <p>You can close this page now</p>
-                </div> :
+            </div> :
                 <div style={{ textAlign: "center" }}>
                     <button style={{ height: "50px", width: "auto", marginTop: "35vh", fontSize: "20px", padding: "5px" }} onClick={handleVerifyLocation}>Verify Location</button>
 
