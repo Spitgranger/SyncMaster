ce78b3fe1b8cc632f1557f65818ec72942433389
Kyle D'Souza <kyle49008@gmail.com>
2025-01-06 22:18:02 -0500

    add backend actions

---
+++ scripts/backend/run_linters.sh
@@ -0,0 +1,10 @@
+echo "checking formatting errors"
+uv run ruff format --check
+echo "checking imports sorted"
+uv run ruff check
+echo "running pylint on source code"
+uv run pylint ./src/backend/
+echo "checking common security vulnerabilities on source code"
+uv run bandit -r ./src/backend/
+echo "running type checker"
+uv run mypy ./src/backend/
