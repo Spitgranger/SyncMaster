eee5344b9632c522ba915a41d0176d7e6020e9ce
RafeedIqbal <iqbalr8@mcmaster.ca>
2025-02-01 06:58:10 -0500

    set up env variabls

--- src/backend/service/environment.py
+++ src/backend/service/environment.py
@@ -12,3 +12,10 @@ DOCUMENT_STORAGE_BUCKET_READ_ROLE = getenv("DOCUMENT_STORAGE_BUCKET_READ_ROLE", 
 
 DOCUMENT_STORAGE_BUCKET_WRITE_ROLE = getenv("DOCUMENT_STORAGE_BUCKET_WRITE_ROLE", "")
 # Role used for writing to the document storage bucket
+
+TARGET_LATITUDE = float(getenv("TARGET_LATITUDE", "43.2588581564085"))
+TARGET_LONGITUDE = float(getenv("TARGET_LONGITUDE", "-79.92097591189501"))
+# Coordinates for ITB
+
+ACCEPTABLE_RADIUS_METERS = int(getenv("ACCEPTABLE_RADIUS_METERS", "100"))
+# radius in which location accepted
