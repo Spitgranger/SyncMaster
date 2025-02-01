e37039c40ef21297eb60e1bca317ae732cd2fc46
RafeedIqbal <iqbalr8@mcmaster.ca>
2025-02-01 07:42:40 -0500

    update route

--- src/backend/service/routes/temp.py
+++ src/backend/service/routes/temp.py
@@ -3,6 +3,10 @@ Temporary route for initial setup
 """
 
 from aws_lambda_powertools.event_handler.api_gateway import Router
+from aws_lambda_powertools.event_handler.openapi.params import Body
+from typing_extensions import Annotated
+from ..location_verification.location_verification import verify_location
+from ..models.location_verification.location_request_response import LocationVerificationRequest
 
 router = Router()
 
@@ -15,3 +19,10 @@ def thingy() -> dict:
     :return: dummy dict.
     """
     return {"Hello": "World"}
+
+@router.post("/verify-location")
+def verify_location_handler(body: Annotated[LocationVerificationRequest, Body()]) -> dict:
+    """
+    Route to verify user location
+    """
+    return verify_location(body)
