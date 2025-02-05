fcc8b03f89ff21cf133b598430618023b987bfca
akshitg3 <akshit.gulia@outlook.com>
2025-02-05 18:06:51 -0500

    Completed the dashboard layout ui

--- src/client-admin-portal/src/pages/dashboard/sitewide.tsx
+++ src/client-admin-portal/src/pages/dashboard/sitewide.tsx
@@ -1,14 +1,25 @@
-import DashboardLayout from '@/components/layouts/DashboardLayout'
+'use clinet'
 import React from 'react'
+import DashboardLayout from '@/components/layouts/DashboardLayout'
+import { Container } from '@mui/material'
+import Grid from '@mui/material/Grid2';
 
 export default function SiteWideDocuments() {
   return (
-    <div>sitewideDocuments</div>
+    <Container maxWidth={"lg"}>
+      <Grid container>
+        <Grid size={12} sx={{
+          width: "100%"
+        }}>
+          Content
+        </Grid>
+      </Grid>
+    </Container>
   )
 }
 
 
 
-SiteWideDocuments.getLayout = function getLayout(page: React.ReactElement){
+SiteWideDocuments.getLayout = function getLayout(page: React.ReactElement) {
   return <DashboardLayout>{page}</DashboardLayout>
 }
