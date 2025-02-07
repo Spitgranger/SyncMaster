f530c2ed2571ebe55a102dea10b1f56e28d4e355
RafeedIqbal <iqbalr8@mcmaster.ca>
2025-02-07 00:13:02 -0500

    Create login.tsx

---
+++ src/client-contractor-portal/src/pages/login.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import React from 'react';
+import { Container } from '@mui/material';
+import SignInForm from '@/components/SignInForm';
+import NonNavbarLogo from '@/components/NonNavbarLogo'
+
+const LoginPage = () => {
+  return (
+    <Container
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        minHeight: '100vh',
+        py: 4,
+      }}
+    >
+      <NonNavbarLogo />
+      <SignInForm />
+    </Container>
+  );
+};
+
+export default LoginPage;
