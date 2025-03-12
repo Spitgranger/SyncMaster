7c5fcc11235174e2eef9d334eb4f26f57115db87
Kyle D'Souza <kyle49008@gmail.com>
2025-03-11 22:59:47 -0400

    add new attributes to schemas

--- src/backend/service/models/api/site_visit.py
+++ src/backend/service/models/api/site_visit.py
@@ -6,6 +6,14 @@ from datetime import datetime
 from typing import Optional
 
 from ..custom_base_model import CustomBaseModel
+from ..db.site_visit import DBFileAttachment
+
+from pydantic import Field
+
+
+class APIFileAttachment(CustomBaseModel):
+    name: str
+    url: str
 
 
 class APISiteVisit(CustomBaseModel):
@@ -14,7 +22,32 @@ class APISiteVisit(CustomBaseModel):
     site_id: str
     user_id: str
     entry_time: datetime
+    allowed_tracking: bool
+    ack_status: bool
     exit_time: Optional[datetime] = None
+    work_order: Optional[str] = None
+    description: Optional[str] = None
+    on_site: Optional[bool] = None
+    attachments: list[APIFileAttachment] = Field(default_factory=list)
+
+
+class APIEnterSiteRequest(CustomBaseModel):
+    """The body of a request to enter a site"""
+
+    allowed_tracking: bool
+    ack_status: bool
+    work_order: Optional[str] = None
+    description: Optional[str] = None
+    on_site: Optional[bool] = None
+
+
+class EditableSiteVisitDetails(CustomBaseModel):
+    """The details of a site visit which are editable"""
+
+    work_order: Optional[str] = None
+    description: Optional[str] = None
+    on_site: Optional[bool] = None
+    attachments: list[DBFileAttachment] = Field(default_factory=list)
 
 
 class APIListSiteVisitResponse(CustomBaseModel):
