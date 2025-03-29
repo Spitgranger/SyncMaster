5eca07b1380c3d337d5529ea2b2138f9a9bebadd
RafeedIqbal <iqbalr8@mcmaster.ca>
2025-03-29 10:34:30 -0400

    Update visitended.tsx

--- src/client-contractor-portal/src/pages/visitended.tsx
+++ src/client-contractor-portal/src/pages/visitended.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+'use client'
+import React from 'react';
+import Container from '@mui/material/Container';
+import { Box, Typography } from '@mui/material';
 
-const visitended = () => {
+const EndVisitPage = () => {
   return (
-    <div>visitended</div>
-  )
-}
+    <Container maxWidth="sm" sx={{ display: 'flex', alignItems: 'center', minHeight: '100vh' }}>
+      <Box
+        sx={{
+          width: '100%',
+          p: 3,
+          boxShadow: 16,
+          textAlign: 'center',
+          borderRadius: 2,
+          mx: 'auto'
+        }}
+      >
+        <Typography variant="h5" gutterBottom>
+          Thank You for Working with Hamilton Water
+        </Typography>
+        <Typography variant="body1">
+          You have been logged out and may now close this tab.
+        </Typography>
+      </Box>
+    </Container>
+  );
+};
 
-export default visitended
+export default EndVisitPage;
